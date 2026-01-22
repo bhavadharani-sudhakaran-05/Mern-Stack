b229699f725eb935b55ec9a06fd554a109a0d6a8
bhavadharani-sudhakaran-05 <ram05mahi03.gk@gmail.com>
2026-01-22 15:30:46 +0530

    Issues

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,13 @@ dotenv.config();
 app.use(express.json());
 app.use('/auth',require('./routes/authRoutes'))
 app.use('/task',require('./routes/taskRoutes'))
-mongoose.connect(process.env.MONGO_URI).then(()=>{console.log("Mongodb connected")}).catch((err)=>{console.log("error occur")});
+mongoose.connect(process.env.MONGO_URI).then(()=>{console.log("Mongodb connected")}).catch((err)=>{console.log(err)});
 app.get('/api',(req,res)=>{res.send("express");});
 app.post('/api',(req,res)=>{
     const temp=req.body;
     res.json(temp);
 })
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
